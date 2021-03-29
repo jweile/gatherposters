7bd0c357aa3a44688d8205d33a5a27751ee7495c
Jochen Weile <jochenweile@gmail.com>
2021-03-29 17:13:14 -0400

    initial commit

---
+++ processPosters.R
@@ -0,0 +1,154 @@
+#!/usr/bin/Rscript
+options(stringsAsFactors=FALSE)
+
+library(yogilog)
+library(yogitools)
+logger <- yogilog::new.logger("posters.log")
+
+#helper function to convert a dataframe into an HTML table body
+df2html <- function(df) {
+	paste(apply(df,1,function(row) {
+		paste0("<tr>",paste(sapply(row, function(field) {
+			paste0("<td>",field,"</td>")
+		}),collapse=" "),"</tr>")
+	}),collapse="\n")
+}
+
+#helper function to draw text with white "shadow" around it
+shadowtext <- function(x, y=NULL, labels, col="black", bg="white", r=0.2, ... ) {
+	theta <- seq(pi/4, 2*pi, length.out=8)
+	xy <- xy.coords(x,y)
+	xo <- r*strwidth('x')
+	yo <- r*strheight('x')
+	for (i in theta) {
+		text( xy$x + cos(i)*xo, xy$y + sin(i)*yo, labels, col=bg, ... )
+	}
+	text(xy$x, xy$y, labels, col=col, ... )
+}
+
+#helper function to generate a overlay label image for gather town icon
+makeOverlay <- function(posternum,res=32,textsize=5) {
+	#prep output image file
+	outfile <- sprintf("processed/overlay_%s.png",posternum)
+	png(outfile,3*res,2*res,res=res)
+	#set graphical parameters to borderless and transparent background
+	op <- par(mar=c(0,0,0,0),bg=NA)
+	#create new canvas and draw text in the center
+	plot.new()
+	shadowtext(0.5,0.6,posternum,cex=textsize)
+	#restore parameters and close image device
+	par(op)
+	invisible(dev.off())
+}
+
+#load poster list
+posterList <- read.csv("posterList.csv")
+#generate tags for each author (<initial>_<lastname>)
+nametag <- tolower(sapply(strsplit(posterList$Presenter," "),function(names){
+	lastname <- tolower(tail(names,1))
+	initial <- tolower(substr(names[[1]],1,1))
+	paste0(initial,"_",lastname)
+}))
+#find matching raw images for each tag
+rawfiles <- sapply(nametag,function(tag) {
+	fs <- list.files("raw",full.names=TRUE,pattern=tag)
+	if (length(fs)==0) {
+		logger$warn("No image found for ",tag)
+		return(NA)
+	} else if (length(fs) > 1) {
+		logger$warn("Multiple matches found for ",tag,": ",paste(fs,collapse=", "))
+		logger$warn("Using first match only!")
+		return(fs[[1]])
+	} else {
+		return(fs)
+	}
+})
+
+#check that all raw images are accounted for
+leftovers <- setdiff(list.files("raw",full.names=TRUE),rawfiles)
+if (length(leftovers)>0) {
+	logger$warn("Unmatched image(s):",paste(leftovers,collapse=", "))
+}
+
+#add file links to table
+posterList$rawfile <- rawfiles
+
+#process images
+yogitools::rowApply(posterList[!is.na(rawfiles),],function(Poster.number,rawfile,...) {
+	logger$info("Processing ",Poster.number)
+	makeOverlay(Poster.number)
+	system2("bash",args=c(
+		"scaleAndComposite.sh",
+		Poster.number,
+		rawfile
+	),wait=TRUE)
+})
+
+
+logger$info("Generating index HTML document")
+
+#generate index document for rooms
+posterList$roomAssignments <- sapply(posterList$Poster.number,substr,1,1)
+
+#generate html listings for each room
+htmlTables <- tapply(1:nrow(posterList),posterList$roomAssignments,function(idx) {
+	room <- posterList[idx[[1]],"roomAssignments"]
+	pnums <- posterList[idx,"Poster.number"]
+	psubnum <- as.integer(substr(pnums,2,nchar(pnums)))
+	subtable1 <- posterList[idx[psubnum <= 8],1:3]
+	subtable2 <- posterList[idx[psubnum > 8],1:3]
+	paste(
+		sprintf("<h1 id=\"%s\">Posters %s</h1>",tolower(room),room),
+		sprintf("<h2 id=\"%s1\">Posters %s1-%s8</h2>",tolower(room),room,room),
+		"<table>",
+		"<tr><th>Poster number</th> <th>Presenter</th> <th>Poster title</th></tr>",
+		df2html(subtable1),
+		"</table>",
+		sprintf("<h2 id=\"%s9\">Posters %s9-%s%d</h2>",tolower(room),room,room,max(psubnum)),
+		"<table>",
+		"<tr><th>Poster number</th> <th>Presenter</th> <th>Poster title</th></tr>",
+		df2html(subtable2),
+		"</table>",
+		sep="\n"
+	)
+})
+
+#html document header
+header <- "<!DOCTYPE html>
+<html>
+<head>
+  <title>Poster Index</title>
+  <style type=\"text/css\">
+    html {
+      font-family: sans-serif;
+    }
+    .main {
+      margin: 1cm;
+      font-size: 120%;
+    }
+    td {
+      padding-bottom: .5ex;
+    }
+    th {
+      text-align: left;
+      background-color: #ccccdd;
+    }
+    table{
+    	width: 100%;
+    }
+  </style>
+</head>
+<body>
+	<div class=\"main\">"
+
+#and footer
+footer <- "
+	</div>
+</body>
+</html>
+"
+
+#write HTML document ot file
+cat(header,paste(htmlTables,collapse="\n"),footer,file="processed/posterList.html")
+
+logger$info("Processing complete!")
---
+++ scaleAndComposite.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#first argument should be the poster number
+posternum=$1
+#second argument should be the poster image
+rawimg=$2
+
+maxsize=3000
+# posternum=B3
+# rawimg=2021-03-19_Church_MSC.png
+
+mkdir -p processed
+
+scaledimg=processed/${posternum}_full.png
+previewimg=processed/${posternum}_preview.png
+thumbimg=processed/${posternum}_thumb.png
+icon=processed/${posternum}_icon.png
+overlay=processed/overlay_${posternum}.png
+
+echo "Checking file..."
+if [[ $(file $rawimg) =~ "image data" ]]; then
+  echo "Image recognized!"
+else
+  >&2 echo "Not an image!"
+  exit 1
+fi
+
+#helper function to check if the image is oversized
+checkSize() {
+  resolution=$(identify -quiet $1|cut -f 3 -d" "|tr x "\n")
+  for x in $resolution; do
+    if [[ $x -ge $maxsize ]]; then
+      return 1
+    fi
+  done
+}
+
+#rescale to max allowed size if necessary
+if ! checkSize $rawimg; then
+	echo "Rescaling..."
+	convert -quiet $rawimg -resize "${maxsize}x${maxsize}" $scaledimg
+else
+	#otherwise we just create a softlink for convenience
+	ln -s $rawimg $scaledimg
+fi
+
+#rescale to preview size
+convert -quiet $scaledimg -resize 800x800 $previewimg
+#rescale to thumbnail size
+convert -quiet $scaledimg -resize 86x54 $thumbimg
+
+echo "Building icon..."
+#determine the final width of the thumbnail
+thumbwidth=$(identify -quiet $thumbimg|cut -f 3 -d" "|sed -Ee 's/x.+//g')
+#and calculate how far off-set it needs to be from the left edge of the board
+#to be centered
+offset=$(( 5+43-($thumbwidth/2) ))
+
+#composite overlay over thumbnail and posterboard tile to generate icon
+composite -quiet -geometry +${offset}+2 $thumbimg posterboard.png $icon
+composite -quiet $overlay $icon $icon
+
+#cleanup:  we don't need the thumbnail and overlay anymore
+rm $thumbimg $overlay
+
+echo "Done!"
