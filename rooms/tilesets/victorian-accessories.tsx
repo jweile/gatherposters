bedda0cc8e9be1a57e3d11c155c20656953903db
Jochen Weile <jochenweile@gmail.com>
2021-04-06 17:39:29 -0400

    readme files and room graphics

---
+++ rooms/tilesets/victorian-accessories.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="victorian-accessories" tilewidth="32" tileheight="32" tilecount="2560" columns="32">
+ <image source="lpc-victorian-buildings/victorian-accessories.png" width="1024" height="2560"/>
+</tileset>
---
+++ rooms/tilesets/victorian-streets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="victorian-streets" tilewidth="32" tileheight="32" tilecount="1536" columns="16">
+ <image source="lpc-victorian-decoration/victorian-streets.png" width="512" height="3072"/>
+</tileset>
