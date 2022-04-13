ab6ff2bab92f8cba0f8e17244d0ddf58dc86556a
Jochen Weile <jochenweile@gmail.com>
2022-04-13 10:24:13 -0400

    integrate attendance survey

--- abstracts.R
+++ abstracts.R
@@ -5,6 +5,14 @@ options(stringsAsFactors=FALSE)
 abstractTable <- read.csv(commandArgs(TRUE)[[1]])
 # abstractTable <- read.csv('MSS22 Poster abstract submission (Responses) - Form Responses 1.csv')
 
+attendanceTable <- read.csv(commandArgs(TRUE)[[2]])
+# attendanceTable <- read.csv('MSS22 Attendance survey (Responses) - Form responses 1.csv')
+attendance <- setNames(attendanceTable[,3],attendanceTable[,2])
+abstractTable$attInPerson <- attendance[abstractTable$Email.Address]
+abstractTable$attInPerson[is.na(abstractTable$attInPerson)] <- "No response"
+
+
+
 #escape latex command characters
 escape <- function(strs) {
   strs <- trimws(strs)
@@ -60,6 +68,7 @@ content <- sapply(1:nrow(abstractTable),function(i) with(abstractTable[i,],{
     sprintf("\\textbf{Presenter:} %s (%s, %s)\\\\",Presenter.Name, Presenter.s.Role, Presenter.s.Institution),
     sprintf("\\textbf{Submitted on:} %s\\\\",Timestamp),
     sprintf("\\textbf{Platform talk:} %s\\\\",Would.you.like.your.abstract.to.be.considered.for.a.platform.talk.),
+    sprintf("\\textbf{In person:} %s\\\\",attInPerson),
     "\\pagebreak"
   )
 }))
@@ -71,6 +80,12 @@ close(con)
 
 #compile latex to pdf
 retVal <- system("pdflatex -halt-on-error abstracts")
+
+notAttributable <- setdiff(names(attendance),abstractTable$Email.Address)
+if (length(notAttributable) > 0) {
+  cat("Non-attributable email address(es):\n")
+  cat(paste(sprintf(" * %s : %s",notAttributable,attendance[notAttributable]),collapse="\n"),"\n")
+}
 
 #cleanup (if compilation was successful)
 if (retVal == 0) {
